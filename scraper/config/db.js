0dadc090ec79489dd4040c62d37fdf989fa10cea
daiant <cmg2512@gmail.com>
2022-01-26 18:14:40 +0100

    added .env for postgresql connection

--- scraper/config/db.js
+++ scraper/config/db.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const { Pool } = require('pg');
+
 
 const credentials = {
     user: "davi", 
     host: "localhost", 
     database: "davi", 
-    password: "davi", 
+    password: process.env.PGPASSWORD, 
     port: 5432
 }
 const pool = new Pool(credentials);
