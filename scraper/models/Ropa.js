c31fe145acb253cb5e69f6a3bdea69e0434a6d22
daiant <cmg2512@gmail.com>
2022-01-26 15:38:33 +0100

    Modelos de ropa, marca y sexo

--- scraper/models/Ropa.js
+++ scraper/models/Ropa.js
@@ -1,9 +1,11 @@
 class Ropa {
-    constructor(nombre, categoria, marca, precio) {
+    constructor(nombre, sexo, categoria, marca, precio, link) {
         this.nombre = nombre;
+        this.sexo = sexo;
         this.categoria = categoria; 
         this.marca = marca; 
         this.precio = precio;
+        this.link = link;
     }
 }
 
